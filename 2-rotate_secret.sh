bcc4ae845f71759cdabe51185834760c24377a2e
Quincy Cheng <quincy.cheng@gmail.com>
2020-07-23 13:33:33 +0800

    Update 2-rotate_secret.sh

--- 2-rotate_secret.sh
+++ 2-rotate_secret.sh
@@ -8,7 +8,7 @@ conjur variable values add \
    pas_dev/app/cityapp-safe/Database-Oracle-10.0.0.20-cityapp/password \
    ${secretVal}
 
-# update Oralce DB
+# update Oracle DB secret
 
 sqlplus system/Cyberark1@10.0.0.20 << EOF
 
